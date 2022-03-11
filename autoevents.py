78c108123bbec261ce56a744ecdaf3025011b79a
hamster007github <you@example.com>
2022-03-11 17:27:24 +0100

    Fix: use default 3 hours for bonus lure duration, if value in event.json from pogoinfo is missing

--- autoevents.py
+++ autoevents.py
@@ -380,7 +380,9 @@ class EventWatcher(mapadroid.utils.pluginBase.Plugin):
 
             for bonus in raw_event["bonuses"]:
                 if bonus.get("template", "") == "longer-lure":
-                    event_dict["lure"] = bonus["value"]*60
+                    # get bonus duration time in hour. If not available use default=3
+                    bonus_lure_duration_hours = bonus.get("value", 3)
+                    event_dict["lure"] = bonus_lure_duration_hours*60
                     break
             if raw_event["has_spawnpoints"] or event_dict.get("lure"):
                 self._spawn_events.append(event_dict)
